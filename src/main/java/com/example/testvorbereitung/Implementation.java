3ef2a76ff1031c7b61b1d057b56d65c52fd6b7bf
Omar <omerhussein2608@hotmail.com>
2023-06-11 14:15:13 +0200

    New branch

---
+++ src/main/java/com/example/testvorbereitung/Implementation.java
@@ -0,0 +1,9 @@
+package com.example.testvorbereitung;
+
+public class Implementation {
+
+    public double YardToMeter(double yd){
+
+        return yd /1.094;
+    }
+}
--- src/test/java/com/example/testvorbereitung/TestvorbereitungApplicationTests.java
+++ src/test/java/com/example/testvorbereitung/TestvorbereitungApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.testvorbereitung;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TestvorbereitungApplicationTests {
 
+	Implementation sut = new Implementation();
 	@Test
-	void contextLoads() {
+	public void testYardToMeter() {
+		var result = sut.YardToMeter(1);
+		var expected = 0.914;
+		var delta=0.001;
+		assertEquals(expected,result, delta);
 	}
 
 }
